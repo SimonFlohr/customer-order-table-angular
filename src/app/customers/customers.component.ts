9a12ad51a6dc64b8f560a42a23c5f581bf1da571
Simon Flohr <sflohr2@wgu.edu>
2023-12-30 01:25:43 -0600

    Create 'Customers' component

---
+++ src/app/customers/customers.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'app-customers',
+    templateUrl: './customers.component.html',
+    styleUrl: './customers.component.css'
+})
+export class CustomersComponent implements OnInit {
+    
+
+    constructor() {
+
+    }
+    ngOnInit() {
+        
+    }
+}
