83f27c68814257fb78636e5b06d55a3cef72e18e
Simon Flohr <sflohr2@wgu.edu>
2024-01-01 06:09:48 -0600

    Create interfaces.ts and shared.module.ts
    Create and implement 'interfaces.ts' and 'shared.module.ts'. Update variable types in several files, in accordance with the newly defined interfaces.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
 import { CustomersModule } from './customers/customers.module';
+import { SharedModule } from './shared/shared.module';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { CustomersModule } from './customers/customers.module';
   imports: [
     BrowserModule,
     CustomersModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [
     provideClientHydration()
